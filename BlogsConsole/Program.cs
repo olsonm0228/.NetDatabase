a8cbf900ddd6cec32812eb29eac2a91fed493eca
Michael <olsonm0228@users.noreply.github.com>
2020-11-05 20:49:28 -0600

    errors added to options 1 and 2

--- BlogsConsole/Program.cs
+++ BlogsConsole/Program.cs
@@ -29,7 +29,7 @@ namespace BlogsConsole
                     Console.WriteLine("Enter q to quit");
                     logger.Info("User input taken");
                     choice = Console.ReadLine();
-
+                    //todo errors!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                     switch(choice){
                         case "1":
                             logger.Info("1) selected");
@@ -37,9 +37,11 @@ namespace BlogsConsole
                             var query = db.Blogs.OrderBy(b => b.Name);
 
                             Console.WriteLine(query.Count() + " Blogs returned");
-                            foreach (var item in query)
-                            {
-                                Console.WriteLine(item.Name);
+                            if(query.Count() > 0){
+                                foreach (var item in query)
+                                {
+                                    Console.WriteLine(item.Name);
+                                }
                             }
                             break;
                         case "2":
@@ -47,17 +49,30 @@ namespace BlogsConsole
                             // Create and save a new Blog
                             Console.Write("Enter a name for a new Blog: ");
                             var name = Console.ReadLine();
+                            if(name.Equals("")){
+                                logger.Error("Blog name cannot be null");
+                            } else{
+                                var blog = new Blog { Name = name };
 
-                            var blog = new Blog { Name = name };
-
-                            db.AddBlog(blog);
-                            logger.Info("Blog added - {name}", name);
+                                db.AddBlog(blog);
+                                logger.Info("Blog added - {name}", name);
+                            }
                             break;
                         case "3":
                             logger.Info("3) selected");
+                            // Display all Blogs from the database
+                            Console.WriteLine("Select the blog you would like to post to:");
+                            var queryBlog = db.Blogs.OrderBy(b => b.BlogId);
+
+                            foreach (var item in queryBlog)
+                            {
+                                Console.WriteLine(item.BlogId +") "+ item.Name);
+                            }
+                            //todo ask what they want to post to and add a post
                             break;
                         case "4":
                             logger.Info("4) selected");
+                            //todo display posts on a blog after they select a blog
                             break;
                         default:
                             logger.Info("User has quit");
@@ -72,10 +87,6 @@ namespace BlogsConsole
             {
                 logger.Error(ex.Message);
             }
-
-            try
-            {
-
 
             logger.Info("Program ended");
         }
